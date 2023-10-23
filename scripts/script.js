0fac382ae3770bd83e20258ddfb74702ad4c7300
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-23 08:19:55 +0200

    Correción de errores

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 // https://github.com/jaimecamocha/tareaTema2DWECDIW.git
 
 const inventario = [
-    { id: 1, nombre: "Camiseta", cantidad: 50, precio: 15 },
+    { id: 1, nombre: "Camiseta", cantidad: 50, precio: 15 }, 
     { id: 2, nombre: "Pantalones", cantidad: 30, precio: 30 },
     { id: 3, nombre: "Zapatos", cantidad: 20, precio: 50 },
 ];
